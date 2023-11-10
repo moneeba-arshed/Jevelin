668d874caa29a73e0d0c62364486b37c2ceb0957
moneeba-arshed <moneebaarshed@example.com>
2023-11-10 15:03:56 +0500

    Structure Folder

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      {/* Hero Section */}
+      {/* Services And Protfolio */}
+      {/* Counter */}
+      {/* Master Crew */}
+      {/* Team */}
+      {/* Cards */}
+      {/* Partners */}
+      {/* Slider */}
+      {/* Newsletter */}
+      {/* Contact Us */}
+      {/* Footer */}
+    </div>
+  )
+}
+
+export default Home
